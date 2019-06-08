a69849f8e47036dd8388cd4e6e80c2e4b91b230d
Ilya <ilanevazno11@gmail.com>
2019-06-09 01:16:33 +0200

    09.06.2019
    Добавлена отрисовка процентов

--- src/slider.js
+++ src/slider.js
@@ -2,6 +2,7 @@ import { get } from "http";
 import { offset } from "highcharts";
 
 $(document).ready(function(e){
+    // Движение pointer по слайдеру
     let slider = $('.slider');
     let toggler = slider.children(0);
     
@@ -28,10 +29,15 @@ $(document).ready(function(e){
             if (newLeft > rightEdge) {
                 newLeft = rightEdge;
             }
+            console.log(Math.floor(newLeft * 100 / rightEdge))
             
             toggler.css({
                 "left": newLeft + "px",
             })
+            //Отрисовка процентов заполненности слайдера
+            let sliderValue = $('.slider_value');
+            sliderValue[0].innerText = Math.floor(newLeft * 100 / rightEdge); 
+            // 
         }
 
         $('body').on('mousemove', function(e){
