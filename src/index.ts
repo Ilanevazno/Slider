14db7c4d972b3cc36933e9fa685371f866eb1195
Ilya <ilanevazno11@gmail.com>
2019-09-09 23:18:24 +0300

    feat: let the hunt begin!

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import * as $ from "jquery";
+import "./index.scss";
 
 namespace Slider {
     interface jQuery{
         mySlider():jQuery;
     }
 
+    class model {
+        slider = slider;
+    }
+
+    class view {
+        constructor(){
+            let sliderBody = $('<div/>', {
+                class: 'slider__body'
+            }).appendTo(slider);
+
+            let sliderPointer = $('<span/>', {
+                class: "slider__pointer"
+            }).appendTo(sliderBody);
+
+            let sliderValue = $('<span/>', {
+                class: "slider__value"
+            }).appendTo(sliderBody);
+        }
+    }
+
+    class controller {
+
+    }
+
     let $ = jQuery;
 
     jQuery.fn.extend({
         createSlider: function(){
-            console.log("asd");
+            slider = new view;
         }
     })
 }
